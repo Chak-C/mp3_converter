b98ce3c95ba398a7f5b46744f522b8ad3be8adbd
AzAcq <114961815+Chak-C@users.noreply.github.com>
2024-03-21 10:53:06 +1100

    Update config.py

--- Current Version/config.py
+++ Current Version/config.py
@@ -9,11 +9,11 @@ CURRENT_TRACK_YEAR = ''
 
 # Youtube
 API_ENDPOINT = 'https://www.googleapis.com/youtube/v3/videos' # Use URL to get video information (length, size, thumbnail, title)
-API_KEY = 'AIzaSyAQcQcyi2CHRo2ly8wWfoZmuecTvp3fLzw' # Enter key
+API_KEY = '' # Enter key
 
 # Spotify
-CLIENT_ID = '43fd0edf8b0e495c81dc1b38cc237b77' # enter id
-CLIENT_SECRET = '49b8247f173447b3a2bf1bda588153c4' # enter secret
+CLIENT_ID = '' # enter id
+CLIENT_SECRET = '' # enter secret
 
 SPOTIFY_API_ENDPOINT = 'https://accounts.spotify.com/api/token'
 SPOTIFY_TOKEN = '' # Valids for 30-60 mins every run
